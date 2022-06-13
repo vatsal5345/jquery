8ff6a726e2bd1945a4ea0161a5c7e50c1e5d8eaa
vatsal5345 <vatsal5345@gmail.com>
2022-06-13 21:10:29 +0530

    add click validation using functions

--- jqury/formjqury.js
+++ jqury/formjqury.js
@@ -1,6 +1,5 @@
-$(document).ready(function(){
-    $("#fname").blur(function(){
-        let fname = $('#fname').val();
+function fname_val(){
+  let fname = $('#fname').val();
         let fname_pattern = "[a-zA-Z]{2,30}$";
 
         if(!fname.match(fname_pattern)){
@@ -8,91 +7,93 @@ $(document).ready(function(){
         text = 'Enter valid First name';
         $('#OP1').text(text); 
         }
-        
-    });
-    $("#lname").blur(function(){
-      let lname = $('#lname').val();
+}
+
+function lname_val(){
+  let lname = $('#lname').val();
       let lname_pattern = "[a-zA-z]{2,30}$";
       if(!lname.match(lname_pattern)){
         let text;
         text = 'Enter valid Last name'
         $('#OP2').text(text);
       }
-    });
-    $("#staddress").blur(function(){
-      let address = $('#staddress').val();
+
+}
+function staddress_val(){
+  let address = $('#staddress').val();
       let address_pattern = "[a-zA-Z0-9]{3,600}$";
       if(!address.match(address_pattern)){
         let text;
         text = 'Enter valid address'
         $('#OPa').text(text);
       }
-    });
-    $("#staddress2").blur(function(){
-      let address = $('#staddress2').val();
+}
+function staddress2_val(){
+  let address = $('#staddress2').val();
       let address_pattern = "[a-zA-Z0-9]{3,600}$";
       if(!address.match(address_pattern)){
         let text;
         text = 'Enter valid address'
         $('#OP3').text(text);
       }
-    });
-    $("#city").blur(function(){
-      let city = $('#city').val();
+}
+function city_val(){
+  let city = $('#city').val();
       let city_pattern = "[a-zA-Z]$";
       if(!city.match(city_pattern)){
         let text;
         text = 'Enter valid city name'
         $('#OP4').text(text);
       }
-    });
-    $("#state").blur(function(){
-      let state = $('#state').val();
+
+}
+function state_val(){
+  let state = $('#state').val();
       let state_pattern = "[a-zA-Z]$";
       if(!state.match(state_pattern)){
         let text;
         text = 'Enter valid state name'
         $('#OP5').text(text);
       }
-    });
-    $("#zipcode").blur(function(){
-      let zipcode = $('#zipcode').val();
+}
+function zipcode_val(){
+  let zipcode = $('#zipcode').val();
       let zipcode_pattern = "^[1-9]{1}[0-9]{5}$";
       if(!zipcode.match(zipcode_pattern)){
         let text;
         text = 'Enter valid 6 digit zipcode'
         $('#OP6').text(text);
       }
-    });
-    $("#phone").blur(function(){
-      let phone = $('#phone').val();
+}
+function phone_val(){
+  let phone = $('#phone').val();
       let phone_pattern = "\(([0-9]{3})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{3})$";
       if(!phone.match(phone_pattern)){
         let text;
         text = 'Enter valid phone number'
         $('#OP7').text(text);
       }
-    });
-    $("#email").blur(function(){
-      let email = $('#email').val();
+}
+function email_val(){
+  let email = $('#email').val();
       let email_pattern = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/";
       if(!email.match(email_pattern)){
         let text;
         text = 'Enter valid Email-id'
         $('#OP8').text(text);
       }
-    });
-    $("#feedback").blur(function(){
-      let feedback = $('#feedback').val();
+}
+function feedback_val(){
+  let feedback = $('#feedback').val();
       let feedback_pattern = ".{3,100}";
       if(!feedback.match(feedback_pattern)){
         let text;
         text = 'please give feedback to us...'
         $('#OP9').text(text);
       }
-    });
-    $("#suggestion").blur(function(){
-      let suggestion = $('#suggestion').val();
+}
+function suggestion_val(){
+  let suggestion = $('#suggestion').val();
       let suggestion_pattern = ".{3,100}";
       if(!suggestion.match(suggestion_pattern)){
         let text;
@@ -100,6 +101,44 @@ $(document).ready(function(){
         $('#OP10').text(text);
         
       }
+}
+
+
+
+
+$(document).ready(function(){
+    $("#fname").blur(function(){
+        fname_val(); 
+    });
+    $("#lname").blur(function(){
+      lname_val();
+    });
+    $("#staddress").blur(function(){
+      staddress_val();
+    });
+    $("#staddress2").blur(function(){
+      staddress2_val();
+    });
+    $("#city").blur(function(){
+      city_val();
+    });
+    $("#state").blur(function(){
+      state_val();
+    });
+    $("#zipcode").blur(function(){
+      zipcode_val();
+    });
+    $("#phone").blur(function(){
+      phone_val();
+    });
+    $("#email").blur(function(){
+      email_val();
+    });
+    $("#feedback").blur(function(){
+      feedback_val();
+    });
+    $("#suggestion").blur(function(){
+      suggestion_val();
     });
     $("#submit").click(function(event) {
       event.preventDefault();    
@@ -109,5 +148,20 @@ $(document).ready(function(){
           $('#genderErr').text(text); 
       }
        
-    });
+  });
+
+  $("#submit").click(function(){
+    fname_val();
+    lname_val();
+    staddress_val();
+    staddress2_val();
+    city_val();
+    state_val();
+    zipcode_val();
+    phone_val();
+    email_val();
+    feedback_val();
+    suggestion_val();
+
+  });
 });
